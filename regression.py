8a06c06259cb5809774568108d649e0e340730cd
Jackson Chen <tiger2chen@gmail.com>
2014-12-25 15:33:41 -0700

    Finishes matrix preprocessing

--- regression.py
+++ regression.py
@@ -4,28 +4,72 @@ import re
 from pylab import *
 import glob
 import string
+from time import sleep
+import sys
+from datetime import datetime
 
-app_comments = pd.DataFrame(columns=['date_published', 'downloads', 'package', 'reviews', 'version_code', 'version_name'])
+def compile_comments(path, app_comments):
+  for fname in glob.glob(path):
+    app_comments = pd.concat([app_comments, pd.read_json(fname)], ignore_index=True)
+  return app_comments
 
-path = "/Users/Jackson/dev/science/health-apps/comments/*.json"
-for fname in glob.glob(path):
-  app_comments = pd.concat([app_comments, pd.read_json(fname)], ignore_index=True)
+def words_table(app_comments):
+  words_per_comment = pd.DataFrame(columns = ['comments', 'words'])
+  words_per_comment.set_index(['comments'], inplace = True)
+  contraction_match = re.compile("[^']+'[a-zA-Z]{1,2}$")
+  total_words = []
+  for index, row in app_comments.iterrows():
+    if row[3]['st'] is not None and row[3]['cmt'] is not None:
+      contractions = [x.encode('ascii','ignore').lower().translate(string.maketrans('', ''), '\\') for x in filter(None, re.split("\s|,|:|-|\.|\(|\)|\/|\[|\]|!|\*|;|\"|\\|\\\\|\+", row[3]['cmt'])) if contraction_match.match(x.encode('ascii','ignore'))]
+      cleaned_words = [x.encode('ascii','ignore').lower().translate(string.maketrans('', ''), '\\!?\.') for x in filter(None, re.split("\s|,|:|-|\.|\(|\)|\/|\[|\]|!|\*|;|\"|'|\\|\\\\|\+", row[3]['cmt']))]
+      cleaned_words = filter(None, cleaned_words)
+      words_per_comment.loc[index, 'words'] = cleaned_words + contractions
+      total_words.extend(cleaned_words)
+      if contractions is not None:
+        total_words.extend(contractions)
 
-regex = re.compile(r'[^()!?]')
-ellipse = re.compile(r'([^.]+)\.+([^.]+)')
-total_words = []
-for index, row in app_comments.iterrows():
-  if row[3]['st'] is not None and row[3]['cmt'] is not None:
-    cleaned_words = [str(x).translate(string.maketrans('', ''), '()?!\"\'\\/').lower() for x in row[3]['cmt'].split()]
-    cleaned_words = list(set(cleaned_words))
-    for x in cleaned_words:
-      if ellipse.match(x):
-        results = ellipse.match(x)
-        cleaned_words = [y for y in cleaned_words if y != x]
-        cleaned_words.extend([results.group(1), results.group(2)])
-      cleaned_words = [z.translate(string.maketrans('', ''), '.') for z in cleaned_words]
+  return list(set(total_words)), words_per_comment
 
-    total_words.extend(cleaned_words)
+def words_comments_table(total_words, comments_table, words_per_comment):
+  words_comments = pd.DataFrame(columns = ['comments'])
+  words_comments.set_index(['comments'], inplace = True)
 
-total_words = list(set(total_words))
-print total_words
+  comments_ratings = pd.DataFrame(columns = ['comments', 'rating'])
+  comments_ratings.set_index(['comments'], inplace = True)
+
+  progress_counter = 0
+  for index, row in comments_table.iterrows():
+    if row[3]['st'] is not None and row[3]['cmt'] is not None:
+      for word in total_words:
+        words_comments.loc[index, word] = words_per_comment.loc[index, 'words'].count(word)
+      comments_ratings.loc[index, 'rating'] = row[3]['st']
+    progress = int(round(progress_counter*190/len(comments_table.index)))
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-190s] %d%%" % ('='*progress, (progress_counter/len(comments_table.index)*100)))
+    sys.stdout.flush()
+    progress_counter += 1
+
+  return words_comments, comments_ratings
+
+### Main ###
+beginning_time = datetime.now()
+
+app_comments = pd.DataFrame(columns = ['date_published', 'downloads', 'package', 'reviews', 'version_code', 'version_name'])
+app_comments = compile_comments("/Users/Jackson/dev/science/health-apps/comments/*.json", app_comments)
+
+total_words, words_per_comment = words_table(app_comments)
+print "Total word count: " + str(len(total_words))
+
+words_comments, comments_ratings = words_comments_table(total_words, app_comments, words_per_comment)
+
+f = open('words_comments.csv', 'wb')
+f.write(words_comments.to_csv(index = False))
+f.close()
+f = open('comments_ratings.csv', 'wb')
+f.write(comments_ratings.to_csv(index = False))
+f.close()
+
+end_time = datetime.now()
+elapsed_seconds = (end_time - beginning_time).seconds
+print ""
+print "Finished after " + '{:02}:{:02}:{:02}'.format(elapsed_seconds // 3600, elapsed_seconds % 3600 // 60, elapsed_seconds % 60)
