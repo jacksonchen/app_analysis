1a2b7c8473dad211a4b38eefada8aa35573825c4
Jackson Chen <tiger2chen@gmail.com>
2015-01-17 20:35:50 -0700

    Print-out

--- pre-regression.py
+++ pre-regression.py
@@ -8,8 +8,12 @@ import sys
 from datetime import datetime
 
 def compile_comments(path, app_comments):
+  counter = 0
   for fname in glob.glob(path):
     app_comments = pd.concat([app_comments, pd.read_json(fname)], ignore_index=True)
+    sys.stdout.write("\r{0}".format(str(counter) + " files read"))
+    sys.stdout.flush()
+    counter += 1
   return app_comments
 
 def words_table(app_comments):
@@ -54,30 +58,36 @@ def words_comments_table(total_words, comments_table, words_per_comment):
   return words_comments, comments_ratings
 
 ## Generates word_comments and comments_ratings tables
-def generate_words_comments():
+def generate_words_comments(words_comments_path, comments_ratings_path, words_per_comment_path):
   app_comments = pd.DataFrame(columns = ['date_published', 'downloads', 'package', 'reviews', 'version_code', 'version_name'])
   app_comments = compile_comments("health-apps/comments/*.json", app_comments)
 
   total_words, words_per_comment = words_table(app_comments)
-  print "Total word count: " + str(len(total_words))
+  print "\nTotal word count: " + str(len(total_words))
 
   words_comments, comments_ratings = words_comments_table(total_words, app_comments, words_per_comment)
 
   ## Writes the dataframes into csv files
-  f = open('health-apps/regression_matrices/words_comments.csv', 'wb')
+  f = open(words_comments_path, 'wb')
   f.write(words_comments.to_csv())
   f.close()
-  f = open('health-apps/regression_matrices/comments_ratings.csv', 'wb')
+  f = open(comments_ratings_path, 'wb')
   f.write(comments_ratings.to_csv())
   f.close()
-  f = open('health-apps/regression_matrices/words_per_comment.csv', 'wb')
+  f = open(words_per_comment_path, 'wb')
   f.write(words_per_comment.to_csv())
   f.close()
 
 ### Main ###
 beginning_time = datetime.now()
 
-generate_words_comments()
+  # Configurations
+f = open('config.py', 'r').readlines()
+words_comments_path = re.match("words_comments_path = '([^']+)'", f[2]).group(1)
+comments_ratings_path = re.match("comments_ratings_path = '([^']+)'", f[4]).group(1)
+words_per_comment_path = re.match("words_per_comment_path = '([^']+)'", f[5]).group(1)
+
+generate_words_comments(words_comments_path, comments_ratings_path, words_per_comment_path)
 
 end_time = datetime.now()
 elapsed_seconds = (end_time - beginning_time).seconds
